9118ad7ed0108d01c70d322df142ca1aba67f6c9
JamesAckah <jamesblay80@outlook.com>
2025-09-15 07:48:07 +0000

    Fix Amplify auth configuration with complete Auth object

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -1,9 +1,24 @@
 const awsconfig = {
+  Auth: {
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_2o9soyelp',
+    userPoolWebClientId: '464fopbqusvc5m9pmjs8cg0g08',
+    mandatorySignIn: false,
+    authenticationFlowType: 'USER_PASSWORD_AUTH'
+  },
+  API: {
+    endpoints: [
+      {
+        name: 'electro-api',
+        endpoint: 'https://ev7foqrqtrb5dfnc5jxzsmncba.appsync-api.us-east-1.amazonaws.com/graphql',
+        region: 'us-east-1'
+      }
+    ]
+  },
   aws_project_region: 'us-east-1',
   aws_cognito_region: 'us-east-1',
   aws_user_pools_id: 'us-east-1_2o9soyelp',
   aws_user_pools_web_client_id: '464fopbqusvc5m9pmjs8cg0g08',
-  aws_cognito_identity_pool_id: 'us-east-1:your-identity-pool-id',
   aws_appsync_graphqlEndpoint: 'https://ev7foqrqtrb5dfnc5jxzsmncba.appsync-api.us-east-1.amazonaws.com/graphql',
   aws_appsync_region: 'us-east-1',
   aws_appsync_authenticationType: 'API_KEY',
