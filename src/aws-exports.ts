f7b83fa9711adc922ad799f478e718269ac68b12
JamesAckah <jamesblay80@outlook.com>
2025-09-15 08:13:27 +0000

    Fix Amplify config with proper Cognito configuration format

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -1,24 +1,19 @@
 const awsconfig = {
-  Auth: {
-    region: 'us-east-1',
-    userPoolId: 'us-east-1_2o9soyelp',
-    userPoolWebClientId: '464fopbqusvc5m9pmjs8cg0g08',
-    mandatorySignIn: false,
-    authenticationFlowType: 'USER_PASSWORD_AUTH'
-  },
-  API: {
-    endpoints: [
-      {
-        name: 'electro-api',
-        endpoint: 'https://ev7foqrqtrb5dfnc5jxzsmncba.appsync-api.us-east-1.amazonaws.com/graphql',
-        region: 'us-east-1'
-      }
-    ]
-  },
   aws_project_region: 'us-east-1',
   aws_cognito_region: 'us-east-1',
   aws_user_pools_id: 'us-east-1_2o9soyelp',
   aws_user_pools_web_client_id: '464fopbqusvc5m9pmjs8cg0g08',
+  oauth: {},
+  aws_cognito_username_attributes: ['email'],
+  aws_cognito_social_providers: [],
+  aws_cognito_signup_attributes: ['email'],
+  aws_cognito_mfa_configuration: 'OFF',
+  aws_cognito_mfa_types: ['SMS'],
+  aws_cognito_password_protection_settings: {
+    passwordPolicyMinLength: 8,
+    passwordPolicyCharacters: []
+  },
+  aws_cognito_verification_mechanisms: ['email'],
   aws_appsync_graphqlEndpoint: 'https://ev7foqrqtrb5dfnc5jxzsmncba.appsync-api.us-east-1.amazonaws.com/graphql',
   aws_appsync_region: 'us-east-1',
   aws_appsync_authenticationType: 'API_KEY',
